ea8550a8c9711d0902a26053b25b5baa528e535d
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-29 14:29:09 -0700

    add middle function return middle of array

--- middle.js
+++ middle.js
@@ -22,19 +22,23 @@ const assertArraysEqual = (actual, expected) => {
 };
 
 const middle = (arr) => {
+  // edge case when arr has less than 3 items
   if (arr.length < 3) {
     return [];
   }
 
-  // get middle index of array for odd number lengths
-  const middle = Math.floor(arr.length);
-  console.log(middle);
+  // get middle index of array
+  const middleIndex = Math.floor(arr.length / 2);
+
+  // if true (1), return first
+  // if false (even length) (0), return second option
+  return arr.length % 2 ? arr[middleIndex] : [arr[middleIndex - 1], arr[middleIndex]];
 };
 
 // test cases
-// console.log(middle([1]));; // => []
-// console.log(middle([1, 2])); // => []
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
 console.log(middle([1, 2, 3])); // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
