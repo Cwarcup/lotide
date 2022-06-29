3824c46cd658a8b144d837d2a0bcd4092270548c
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-29 14:14:25 -0700

    add raisin function

--- middle.js
+++ middle.js
@@ -25,12 +25,16 @@ const middle = (arr) => {
   if (arr.length < 3) {
     return [];
   }
+
+  // get middle index of array for odd number lengths
+  const middle = Math.floor(arr.length);
+  console.log(middle);
 };
 
 // test cases
-console.log(middle([1]));; // => []
-// middle([1, 2]) // => []
-// middle([1, 2, 3]) // => [2]
+// console.log(middle([1]));; // => []
+// console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
 // middle([1, 2, 3, 4, 5]) // => [3]
 // middle([1, 2, 3, 4]) // => [2, 3]
 // middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
