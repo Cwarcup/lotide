a7080c4dd982dd3070f381a47de571f8cc300ee2
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-29 10:13:16 -0700

    add middle

---
+++ middle.js
@@ -0,0 +1,36 @@
+// assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// used for testing purposes
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const middle = (arr) => {
+  if (arr.length < 3) {
+    return [];
+  }
+};
+
+// test cases
+console.log(middle([1]));; // => []
+// middle([1, 2]) // => []
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+// middle([1, 2, 3, 4]) // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
