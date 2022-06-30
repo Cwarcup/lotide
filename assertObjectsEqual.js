a5945859c47e4bd7130ebf940d50bc1555af52d5
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-29 18:50:12 -0700

    add assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -36,21 +36,16 @@ const eqObjects = (object1, object2) => {
   return true;
 };
 
-//handling arrays
+// Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console.
 
-const cd = {c: '1', d: ['2', 3]};
-const dc = {d: ['2', 3], c: '1'};
+const assertObjectsEqual = (actual, expected) => {
+  const inspect = require('util').inspect;
 
-console.log(eqObjects(cd, dc)); // => true
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${inspect(actual)} === [${inspect(expected)}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+  }
+};
 
-const cd2 = {c: '1', d: ['2', 3, 4]};
-console.log(eqObjects(cd, cd2)); // => false
-
-const ab = {a: '1', b: '2'};
-const ba = {b: '2', a: '1'};
-// assertEqual(ab.a, ba.a); // true statement
-
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = {a: '1', b: '2', c: '3'};
-console.log(eqObjects(ab, abc)); // => false
+// assertObjectsEqual({a: '1', b: 2}, {b: 2, a: '1'});
