d4ddd215ee5296b311f603bba6f5137489dc9c2a
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-30 14:55:09 -0700

    add takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,22 @@
+// Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+const takeUntil = (arr, cb) => {
+  const result = [];
+
+  for (let item of arr) {
+    if (!cb(item)) {
+      result.push(item);
+    } else {
+      return result;
+    }
+  }
+};
+
+// testing
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1); // [ 1, 2, 5, 7, 2 ]
+
+const data2 = ["I've", 'been', 'to', 'Hollywood', ',', "I've", 'been', 'to', 'Redwood'];
+const results2 = takeUntil(data2, (x) => x === ',');
+console.log(results2); // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
