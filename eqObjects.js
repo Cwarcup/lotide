7c6fb29cba04046828309872dba6f647241cfa21
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-29 18:41:46 -0700

    eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,23 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Returns true if both objects have identical keys with identical values.
@@ -16,24 +30,37 @@ const eqObjects = (object1, object2) => {
 
   // if object keys differ in number
   if (keysObj1.length !== keysObj2.length) {
-    return console.log(false);
+    return false;
   }
 
   // iterate through one of the keys arr
-  for (let key in keysObj1) {
-    // if keys of each object do not match
-    if (object1[key] !== object2[key]) {
-      return console.log(false);
+  for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
   }
-  return console.log(true);
+  return true;
 };
+
+//handling arrays
+
+const cd = {c: '1', d: ['2', 3]};
+const dc = {d: ['2', 3], c: '1'};
+
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = {c: '1', d: ['2', 3, 4]};
+console.log(eqObjects(cd, cd2)); // => false
 
 const ab = {a: '1', b: '2'};
 const ba = {b: '2', a: '1'};
-assertEqual(ab.a, ba.a); // true statement
+// assertEqual(ab.a, ba.a); // true statement
 
-eqObjects(ab, ba); // => true
+console.log(eqObjects(ab, ba)); // => true
 
 const abc = {a: '1', b: '2', c: '3'};
-eqObjects(ab, abc); // => false
+console.log(eqObjects(ab, abc)); // => false
