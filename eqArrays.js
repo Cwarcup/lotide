ade777f5b7df47bae5723142696efc41581d74d9
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-03 16:21:17 -0700

    recursive eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,14 +8,39 @@ const assertEqual = function (actual, expected) {
 
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
+// recursive function to check if two objects are equal
 const eqArrays = (arr1, arr2) => {
+  // base case
   if (arr1.length !== arr2.length) {
     return false;
   }
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    // check if both elements are arrays
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        // if they are arrays, call the eqArrays function
+        return false;
+      }
+      // if they are not arrays, this condition
+    } else if (arr1[i] !== arr2[i]) {
+      // check if characters are equal
       return false;
     }
   }
+  // if all elements are equal, return true
   return true;
 };
+
+// iterative solution
+// const eqArrays = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
