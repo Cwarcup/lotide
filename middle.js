3ad7fe0ed1edb778d247c2bc3191f911099a9e5f
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-04 13:55:34 -0700

    add exports for middle and middleTest

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-// assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// used for testing purposes
-const assertArraysEqual = (actual, expected) => {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const middle = (arr) => {
   // edge case when arr has less than 3 items
   if (arr.length < 3) {
@@ -32,13 +9,15 @@ const middle = (arr) => {
 
   // if true (1), return first
   // if false (even length) (0), return second option
-  return arr.length % 2 ? arr[middleIndex] : [arr[middleIndex - 1], arr[middleIndex]];
+  return arr.length % 2 ? [arr[middleIndex]] : [arr[middleIndex - 1], arr[middleIndex]];
 };
 
-// test cases
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
+
+// console.log(middle([1]), []); // => []
+// console.log(middle([1, 2]), []); // => []
+// console.log(middle([1, 2, 3])); // => [2]
 console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+module.exports = middle;
--- tail.js
+++ tail.js
@@ -1,25 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // function to return everything but the first item of an array
 const tail = function(array) {
   return array.slice(1);
 };
 
 module.exports = tail;
-
-
-// test cases
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
-
-// const words = ["Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
