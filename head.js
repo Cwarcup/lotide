c7a1be8c72b075b846f8557b01c0dee2ae6f83bf
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-04 13:23:09 -0700

    headTest created to test assertion

--- head.js
+++ head.js
@@ -1,11 +1,8 @@
 const assertEqual = require('./assertEqual');
 
 // function to return the first item of an array
-const head = function (array) {
+const head = function(array) {
   return array[0];
 };
 
-// test cases
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['first', 'second', 'third']), 'first');
-assertEqual(head([]), undefined);
+module.exports = head;
