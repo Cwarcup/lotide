cc177ef226e8bdd673ca6b09ce83dbd05998b6ee
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-04 14:25:16 -0700

    refactor tailTest with Mocha and Chai

--- middle.js
+++ middle.js
@@ -1,8 +1,6 @@
 const middle = (arr) => {
   // edge case when arr has less than 3 items
-  if (arr.length < 3) {
-    return [];
-  }
+  if (arr.length < 3) return [];
 
   // get middle index of array
   const middleIndex = Math.floor(arr.length / 2);
@@ -11,13 +9,5 @@ const middle = (arr) => {
   // if false (even length) (0), return second option
   return arr.length % 2 ? [arr[middleIndex]] : [arr[middleIndex - 1], arr[middleIndex]];
 };
-
-
-// console.log(middle([1]), []); // => []
-// console.log(middle([1, 2]), []); // => []
-// console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-// console.log(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
 module.exports = middle;
