725dfd055e0789067953206c5534173173121a4a
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-04 10:02:57 -0700

    add assertEqualTest

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(1, 100);
+assertEqual('identical strings', 'identical strings');
+assertEqual('identical strings', 'different strings');
